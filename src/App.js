36457bd5d3593c191394b14e68e033643d47d652
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2021-04-18 07:57:15 +0200

    marmelade

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hej Bamse, har du honning?
+          Hej Bamse, har du marmelade?
         </p>
         <a
           className="App-link"
